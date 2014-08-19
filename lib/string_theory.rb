fdaba3b185e9d068c3afa3f24ba40a2defb96c9c
Katie Hoffman <ktahoffman@gmail.com>
2014-08-19 12:32:57 -0400

    remove code

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,17 +1,3 @@
-def sort_by_appearance(chars, text)
-  # make hash where each char is the key, count is value
-  count_hash = {}
-  final_string = ""
-  chars.split("").each do |char| 
-    count_hash[char] = 0
-  end
-  # go through each character in text and increment 
-  i = 0
-  text.length.times do
-    count_hash[text[i]] = count_hash[text[i]] + 1 if count_hash[text[i]]
-    i += 1
-  end
-  # sort hash then add to final string
-  count_hash.sort {|a1,a2| a2[1] <=> a1[1] }.each  {|arr| final_string << arr[0] }
-  final_string
+def sort_by_appearance(characters, text)
+  # code goes here
 end
