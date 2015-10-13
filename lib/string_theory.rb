daf0d10f8716af9fab896eb54906b1548e2debd6
zonika <zchodosh@conncoll.edu>
2015-10-13 14:28:37 -0400

    done

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  chars = {}
+  ch = characters_to_sort.split('')
+  long = long_text.split('')
+  ch.each do |c|
+    chars[c] = 0
+  end
+  long.each do |letter|
+    if ch.include? letter
+      chars[letter]+=1
+    end
+  end
+  chars.sort_by{|k,v|v}.reverse.to_h.keys.join
 end
